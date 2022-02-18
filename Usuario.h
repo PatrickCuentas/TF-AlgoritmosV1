4d104770f41781a232562b7f258921b87efdfed1
Patrick <74751873+PatrickCuentas@users.noreply.github.com>
2022-02-18 00:58:55 -0500

    Update Usuario.h

--- Usuario.h
+++ Usuario.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Transaccion.h"
+#include "double.h"
 using namespace std;
 struct Usuario
 {
@@ -12,4 +13,34 @@ struct Usuario
         transaccionesRecibidas = new DoubleList<Transaccion>();
         nombre = _nombre;
     }
+
+    string getNombre() { return this->nombre; }
+
+    void ingresarTransaccionEmitida(Transaccion trans){
+        transaccionesEmitidas->push_back(trans);
+    }
+    void ingresarTransaccionRecibida(Transaccion trans){
+        transaccionesRecibidas->push_back(trans);
+    }
+
 };
+
+ostream& operator<<(ostream& os, Usuario usuario) {
+    string Emitidos = "";
+    string Recibidos = "";
+
+    DoubleList<Transaccion>::Iterator it = usuario.transaccionesEmitidas->begin();
+    for (it; it != usuario.transaccionesEmitidas->end(); ++it) {
+        Emitidos.append((*it).getString());
+        Emitidos.append("\n");
+    }
+
+    DoubleList<Transaccion>::Iterator it2 = usuario.transaccionesRecibidas->begin();
+    for (it2; it2 != usuario.transaccionesRecibidas->end(); ++it2) {
+        Recibidos.append((*it2).getString());
+        Recibidos.append("\n");
+    }
+    
+    os << "Usuario: " << usuario.nombre << "\n" << "Transacciones Emitidas:\n" << Emitidos<<"\nTransacciones Recibidas\n" << Recibidos;
+    return os;
+}
